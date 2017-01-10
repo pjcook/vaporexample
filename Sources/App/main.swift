23c458b2e2902229b5fe03837860d890aa807904
PJ <pj.cook@akqa.com>
2017-01-10 12:47:01 +0000

    First commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,20 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get("/") { request in
+    return "Hello World!"
+}
+
+drop.get("/name", ":name") { request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
+}
+
+drop.get("/view") { request in
+    return try drop.view.make("view.html")
+}
+
+drop.run()
